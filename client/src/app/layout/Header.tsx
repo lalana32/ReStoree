deb43c08f7d1df17c4b99e7d1216c492fa8b342b
Stefan Lalovic <slalovic54@gmail.com>
2024-01-05 10:57:27 +0100

    End of Section 9

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -11,8 +11,9 @@ import {
 } from '@mui/material';
 import { Box } from '@mui/system';
 import { Link, NavLink } from 'react-router-dom';
-import { useStoreContext } from '../context/StoreContext';
+
 import { useAppSelector } from '../store/configureStore';
+import SignedInMenu from './SignedInMenu';
 
 const midLinks = [
   { title: 'catalog', path: '/catalog' },
@@ -44,6 +45,7 @@ interface Props {
 
 export default function Header({ handleThemeChange, darkMode }: Props) {
   const { basket } = useAppSelector((state) => state.basket);
+  const { user } = useAppSelector((state) => state.account);
   const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
@@ -83,14 +85,22 @@ export default function Header({ handleThemeChange, darkMode }: Props) {
               <ShoppingCart />
             </Badge>
           </IconButton>
-
-          <List sx={{ display: 'flex' }}>
-            {rightLinks.map(({ title, path }) => (
-              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
-                {title.toUpperCase()}
-              </ListItem>
-            ))}
-          </List>
+          {user ? (
+            <SignedInMenu />
+          ) : (
+            <List sx={{ display: 'flex' }}>
+              {rightLinks.map(({ title, path }) => (
+                <ListItem
+                  component={NavLink}
+                  to={path}
+                  key={path}
+                  sx={navStyles}
+                >
+                  {title.toUpperCase()}
+                </ListItem>
+              ))}
+            </List>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
